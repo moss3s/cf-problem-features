b5ba5fcdf1dc510cc34671083f2cafa250e4d2f0
moss3s <moss3s@mail.ru>
2018-12-27 20:51:47 +0300

    extend menu for codeforces.com/contest/*/problem/*

--- userscript.js
+++ userscript.js
@@ -2,11 +2,13 @@
 // @id           cf-problem-features
 // @name         Codeforces, problem-page features
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  This userscript adds link from problem to its statistic
 // @author       moss3s
 // @match        http://codeforces.com/problemset/problem/*
 // @match        https://codeforces.com/problemset/problem/*
+// @match        http://codeforces.com/contest/*/problem/*
+// @match        https://codeforces.com/contest/*/problem/*
 // @grant        none
 // ==/UserScript==
 
@@ -17,14 +19,19 @@ function htmlToNode(html) {
 }
 
 (function() {
-    var path = window.location.pathname.split('/').slice(-2)
-    var problemId = path.pop()
-    var contestId = path.pop()
+    // path = .../contest/46/problem/A or .../problemset/problem/46/A
+    var path = window.location.pathname.split('/')
+    const contestPage = path.includes("contest")
+    path = path.slice(-3)
+    var problemId = path.pop();
+    if (contestPage) {
+        path.pop();
+    }
+    var contestId = path.pop();
     console.log(`cf-problem-features: contestId=${contestId} problemId=${problemId}`);
 
+    // newMenu = menu - jquery.lavalamp service node + new menu item
     var menu = document.getElementsByClassName("second-level-menu-list")[0];
-
-    // newMenu = menu - jquery.lavalamp.example service node + new menu item
     var newMenu = menu.cloneNode(true);
     newMenu.removeChild(newMenu.firstChild);
     newMenu.appendChild(htmlToNode(`<li><a href="/problemset/status/${contestId}/problem/${problemId}">Statistic</a></li>`));
